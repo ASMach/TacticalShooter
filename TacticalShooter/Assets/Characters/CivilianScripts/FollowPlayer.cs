c659822987b6c4948d202a5f5c773cb23b5f254c
Alonzo Machiraju <machthree@gmail.com>
2020-02-21 23:27:49 -0800

    Follow player added
    Hostages can follow the player when directed.

---
+++ TacticalShooter/Assets/Characters/CivilianScripts/FollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    bool follow = false;
+
+    GameObject player;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Toggle player follow
+        if (Input.GetButton("Action") && Vector3.Distance(transform.position, player.transform.position) < 2.0f)
+        {
+            follow = !follow;
+        }
+        if (follow)
+        {
+            agent.destination = player.transform.position;
+        }
+    }
+}
